52a5a205e6e644541a822c8a6bc9c030919582ac
Kirt Registe <kirtregiste@gmail.com>
2019-05-16 11:36:02 -0500

    completed blog and polish

--- blog/models.py
+++ blog/models.py
@@ -6,6 +6,17 @@ class Blog(models.Model):
     body = models.TextField()
     image = models.ImageField(upload_to='images/blog_image')
 
+    def __str__(self):
+        return self.title
+
+    def summary(self):
+        return self.body[:100]
+
+    def pub_date_pretty(self):
+        return self.pub_date.strftime('%b %e %Y')
+
+
+
 
 
 #Create a Blog models
